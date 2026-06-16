e25d4cdeee7adf123db83e0376ee5e1220599b22
kcancurly <karadumankcan@gmail.com>
2026-06-16 20:17:57 +0300

    u

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "impacket==0.9.24",
-        "setuptools"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
